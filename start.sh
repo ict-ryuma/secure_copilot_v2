56c514615ce9ea827ed675dda3198a255700dab6
MAYEEN <uddin.md.mayeen@ict.inc>
2025-07-24 18:08:19 +0900

    Docker added

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
-uvicorn backend.chat_secure_gpt:app --host 0.0.0.0 --port 8000 &
+# Copy .env.example to .env if .env does not exist
+if [ ! -f /app/.env ]; then
+  echo "No .env file found. Copying .env.example to .env..."
+  cp /app/.env.example /app/.env
+fi
+uvicorn backend.chat_secure_gpt:app --host 0.0.0.0 --port 8000 --reload &
 
 streamlit run frontend/app.py --server.port 8501 --server.address 0.0.0.0
 
